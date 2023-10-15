0579d9040af3226fd758fa9b28e249921d86f2f3
KevinNevile <kevin.nevileO@gmail.com>
2023-10-14 23:50:02 -0300

    finalizando login, ligando home com login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,10 +1,16 @@
 <template>
-  <q-page class="q-pa-xl">
+  <q-page class="q-pa-xl login-background">
+    <div class="q-mb-md">
+      <q-btn flat round icon="arrow_back" @click="$router.push('/')"
+        >Voltar</q-btn
+      >
+    </div>
+
     <div class="row justify-center">
       <div class="col-12 col-md-6">
         <div class="text-h4 text-bold q-mt-md q-mv-xl">Realizar Login</div>
         <div class="text-subtitle1 text-grey-6 q-mt-md q-mv-xl">
-          Se você já possue um cadastro, faça login com seu email e senha.
+          Se você já possui o cadastro, faça login com seu email e senha.
         </div>
         <div class="q-my-xl text-grey-7">
           <h7>Endereço de Email</h7>
@@ -71,56 +77,8 @@ export default {
 };
 </script>
 
-<!-- <q-card
-      style="
-        width: 300px;
-        height: auto;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      "
-    >
-      <q-card-section class="q-pt-none full-width">
-        <q-input filled v-model="login.email" label="Email" />
-        <q-input
-          class="q-mt-sm"
-          v-model="login.password"
-          filled
-          label="Password"
-          :type="login.isPwd ? 'password' : 'text'"
-        >
-          <template v-slot:append>
-            <q-icon
-              :name="login.isPwd ? 'visibility_off' : 'visibility'"
-              class="cursor-pointer"
-              @click="login.isPwd = !login.isPwd"
-            />
-          </template>
-        </q-input>
-      </q-card-section>
-
-      <q-card-actions class="full-width">
-        <q-btn
-          class="q-mx-sm"
-          color="primary"
-          label="Login"
-          @click="$router.push({ name: 'home' })"
-          style="width: 100%"
-        />
-      </q-card-actions>
-      <q-card-actions class="full-width">
-        <q-btn
-          flat
-          no-caps
-          class="q-mx-sm"
-          color="primary"
-          label="Esqueci minha senha"
-          @click="$router.push({ name: 'esqueciSenha' })"
-          style="width: 100%"
-        />
-      </q-card-actions>
-    </q-card>
-
-    <q-card class="my-card">
-      <div class="absolute-bottom text-subtitle2 text-center">Title</div>
-    </q-card> -->
+<style scoped>
+.login-background {
+  background-color: #ffffff;
+}
+</style>
